182b028c4db4b9348ef8e2806165e059490b42b9
Maciel Alves Pereira <catce.2023111mtds0003@aluno.ifpi.edu.br>
2023-09-19 08:37:12 -0300

    feat: correção gramatical

---
+++ bike.py
@@ -0,0 +1,21 @@
+class Bike:
+    # aros = [12, 16, 20, 24, 26, 29]
+    # vel_max = [15, 20, 25, 30, 60, 70] velocidades imaginárias
+    V_MAX = 20  # km/h
+    V_MIN = 0  # km/h
+    veloc_atual = 0
+    altura_cela = 0
+    calibragem_peneus = None
+
+    def __init__(self, ):
+        pass
+
+    def pedalar(self):
+        pass
+
+    def regular_cela(self):
+        pass
+
+    def calibrar_peneus(self):
+        pass
+
